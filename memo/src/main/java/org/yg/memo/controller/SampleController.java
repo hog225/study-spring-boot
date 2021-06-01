47bcffb452d76d490a40472262988e8eacc4ebe5
yg <epikhighjin@gmail.com>
2021-06-01 20:07:31 +0900

    Memo update 책

--- memo/src/main/java/org/yg/memo/controller/SampleController.java
+++ memo/src/main/java/org/yg/memo/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -25,7 +26,7 @@ public class SampleController {
     
   }
   
-  @GetMapping("/ex2")
+  @GetMapping({"/ex2", "/exLink"})
   public void ex2(Model model){
     
     List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(
@@ -39,7 +40,36 @@ public class SampleController {
       }).collect(Collectors.toList());
     model.addAttribute("list", list);
   }
+
   
+  @GetMapping("/exInline")
+  public String exInline(RedirectAttributes redirectAttributes){
+    log.info("ex inline ....");
+
+    SampleDTO dto = SampleDTO.builder()
+      .sno(10L)
+      .first("first..10")
+      .last("first..10")
+      .regTime(LocalDateTime.now())
+      .build();
+
+    redirectAttributes.addFlashAttribute("result", "success");
+    redirectAttributes.addFlashAttribute("dto", dto);
+
+    return "redirect:/sample/ex3";
+
+  }
+
+  @GetMapping("/ex3")
+  public void ex3(){
+    log.info("ex3");
+  }
+
+  @GetMapping("/exLayout1")
+  public void exLayout1(){
+    log.info("exLayout ..........");
+  }
+
 
 
 }
