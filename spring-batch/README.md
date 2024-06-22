b3a012d4c9ccba161e1660f2c95f8fb176dfc77a
yeonggi <epikhighjin@gmail.com>
2024-06-22 18:14:06 +0900

    readme 수정

--- spring-batch/README.md
+++ spring-batch/README.md
@@ -3,7 +3,7 @@
 2. 해당 내용 참고 - https://github.com/spring-academia/1st-open/tree/main/Spring%20Batch%EB%A5%BC%20%ED%99%9C%EC%9A%A9%ED%95%9C%20%EB%8D%B0%EC%9D%B4%ED%84%B0%20%EC%9D%BC%EA%B4%84%20%EC%B2%98%EB%A6%AC
 
 ## JOB
-
+- [JobScheduler.java](src%2Fmain%2Fjava%2Forg%2Fyg%2Fpractivce%2Fspringbatch%2Fapplication%2Fjob%2FJobScheduler.java) 에서 실행 가능 
 ### meterJob
 
 ### meterTaskletJob
--- spring-batch/src/main/java/org/yg/practivce/springbatch/application/job/JobScheduler.java
+++ spring-batch/src/main/java/org/yg/practivce/springbatch/application/job/JobScheduler.java
@@ -17,6 +17,7 @@ public class JobScheduler {
     private final JobLauncher jobLauncher;
     private final Job meterJob;
     private final Job meterTaskletJob;
+    private final Job createArticleJob;
 
     @Scheduled(fixedDelay = 1000000, initialDelay = 100000000)
     public void runMeterJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -27,12 +28,21 @@ public class JobScheduler {
         jobLauncher.run(meterJob, jobParametersBuilder.toJobParameters());
     }
 
-    @Scheduled(fixedDelay = 1000000, initialDelay = 10)
+    @Scheduled(fixedDelay = 1000000, initialDelay = 100000)
     public void runMeterTaskletJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         System.out.println("Meter Tasklet Job");
         JobParametersBuilder jobParametersBuilder = new JobParametersBuilder()
                 .addString("datetime", String.valueOf(System.currentTimeMillis()));
 
         jobLauncher.run(meterTaskletJob, jobParametersBuilder.toJobParameters());
+    }
+
+    @Scheduled(fixedDelay = 1000000, initialDelay = 10)
+    public void runCreateArticleJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        System.out.println("Create Article Job");
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder()
+                .addString("datetime", String.valueOf(System.currentTimeMillis()));
+
+        jobLauncher.run(createArticleJob, jobParametersBuilder.toJobParameters());
     }
 }
